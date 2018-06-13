54708066bd74bd7df2bc2f06179902960c940966
Fábio Gouveia <hpgroods@gmail.com>
2018-06-13 17:48:41 +0100

    feat : add product entry base column
    Signed-off-by: Fábio Gouveia <hpgroods@gmail.com>

--- app/src/main/java/com/example/android/inventoryapp/data/InventoryContract.java
+++ app/src/main/java/com/example/android/inventoryapp/data/InventoryContract.java
@@ -1,14 +1,19 @@
 package com.example.android.inventoryapp.data;
 
 import android.net.Uri;
+import android.provider.BaseColumns;
 
 public class InventoryContract {
 
     static final String CONTENT_AUTHORITY = "com.example.android.inventoryapp.data";
     static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
-    static final String PATH_PRODUCTS = "products";
-    static final String PATH_PRODUCTS_ID = "products_id";
+    static final String PATH_PRODUCT = "product";
+    static final String PATH_PRODUCT_ID = "product_id";
 
     private InventoryContract() {
+    }
+
+    public static final class ProductEntry implements BaseColumns {
+
     }
 }
