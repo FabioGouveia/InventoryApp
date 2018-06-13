1bdc6d76533a405fe8af9331aaa957e8fd15a864
Fábio Gouveia <hpgroods@gmail.com>
2018-06-13 17:18:44 +0100

    fix : change class package
    Signed-off-by: Fábio Gouveia <hpgroods@gmail.com>

--- app/src/main/java/com/example/android/inventoryapp/ui/MainActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/ui/MainActivity.java
@@ -1,7 +1,7 @@
-package com.example.android.inventoryapp;
+package com.example.android.inventoryapp.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.example.android.inventoryapp.R;
 
