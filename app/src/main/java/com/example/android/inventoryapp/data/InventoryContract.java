2a3f5fce3552a8b11563820f4d1730360fd7be77
Fábio Gouveia <hpgroods@gmail.com>
2018-06-13 17:46:07 +0100

    feat : add static members
    Signed-off-by: Fábio Gouveia <hpgroods@gmail.com>

--- app/src/main/java/com/example/android/inventoryapp/data/InventoryContract.java
+++ app/src/main/java/com/example/android/inventoryapp/data/InventoryContract.java
@@ -1,4 +1,11 @@
 package com.example.android.inventoryapp.data;
 
+import android.net.Uri;
+
 public class InventoryContract {
+
+    static final String CONTENT_AUTHORITY = "com.example.android.inventoryapp.data";
+    static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    static final String PATH_PRODUCTS = "products";
+    static final String PATH_PRODUCTS_ID = "products_id";
 }
