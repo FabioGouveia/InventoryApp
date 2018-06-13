e39303aa65029ab67510dc62155e6a46caa4a625
Fábio Gouveia <hpgroods@gmail.com>
2018-06-13 18:32:27 +0100

    feat : add table create statement
    Signed-off-by: Fábio Gouveia <hpgroods@gmail.com>

--- app/src/main/java/com/example/android/inventoryapp/data/InventoryDbHelper.java
+++ app/src/main/java/com/example/android/inventoryapp/data/InventoryDbHelper.java
@@ -4,6 +4,14 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import static com.example.android.inventoryapp.data.InventoryContract.ProductEntry.COLUMN_PRODUCT_NAME;
+import static com.example.android.inventoryapp.data.InventoryContract.ProductEntry.COLUMN_PRODUCT_PRICE;
+import static com.example.android.inventoryapp.data.InventoryContract.ProductEntry.COLUMN_PRODUCT_QUANTITY;
+import static com.example.android.inventoryapp.data.InventoryContract.ProductEntry.COLUMN_PRODUCT_SUPPLIER_NAME;
+import static com.example.android.inventoryapp.data.InventoryContract.ProductEntry.COLUMN_PRODUCT_SUPPLIER_PHONE_NUMBER;
+import static com.example.android.inventoryapp.data.InventoryContract.ProductEntry.TABLE_NAME;
+import static com.example.android.inventoryapp.data.InventoryContract.ProductEntry._ID;
+
 public class InventoryDbHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "inventory.db";
@@ -16,10 +24,25 @@ public class InventoryDbHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
 
+        String SQL_CREATE_PRODUCTS_TABLE = "CREATE TABLE " + TABLE_NAME + " ("
+                + _ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + COLUMN_PRODUCT_NAME + " TEXT NOT NULL, "
+                + COLUMN_PRODUCT_PRICE + " REAL NOT NULL, "
+                + COLUMN_PRODUCT_QUANTITY + " INTEGER NOT NULL, "
+                + COLUMN_PRODUCT_SUPPLIER_NAME + " TEXT NOT NULL, "
+                + COLUMN_PRODUCT_SUPPLIER_PHONE_NUMBER + " TEXT NOT NULL);";
+
+        db.execSQL(SQL_CREATE_PRODUCTS_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
 
+    @Override
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
     }
 }
