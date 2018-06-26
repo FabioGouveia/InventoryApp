5be2bbe33583a22204b8a53257d112f8521bb698
Fábio Gouveia <hpgroods@gmail.com>
2018-06-26 06:37:04 +0100

    feat : add content type constants
    Signed-off-by: Fábio Gouveia <hpgroods@gmail.com>

--- app/src/main/java/com/example/android/inventoryapp/data/InventoryContract.java
+++ app/src/main/java/com/example/android/inventoryapp/data/InventoryContract.java
@@ -1,19 +1,29 @@
 package com.example.android.inventoryapp.data;
 
+import android.content.ContentResolver;
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public class InventoryContract {
+
+    static final String CONTENT_AUTHORITY = "com.example.android.inventoryapp.inventory";
+    static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    static final String PATH_PRODUCTS = "products";
 
     private InventoryContract() {
     }
 
     public static final class ProductEntry implements BaseColumns {
 
-        public static final String TABLE_NAME = "products";
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PRODUCTS);
         public static final String COLUMN_PRODUCT_NAME = "product_name";
         public static final String COLUMN_PRODUCT_PRICE = "product_price";
         public static final String COLUMN_PRODUCT_QUANTITY = "product_quantity";
         public static final String COLUMN_PRODUCT_SUPPLIER_NAME = "product_supplier_name";
         public static final String COLUMN_PRODUCT_SUPPLIER_PHONE_NUMBER = "product_supplier_phone_number";
+        static final String CONTENT_LIST_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PRODUCTS;
+        static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PRODUCTS;
+        static final String TABLE_NAME = "products";
+        static final String _ID = BaseColumns._ID;
     }
 }
